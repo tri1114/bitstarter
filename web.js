72a33904ea3b1c84b8e72150da0de734deab95f3
Tri Sanguanbun <trirhats@gmail.com>
2013-07-12 07:31:43 +0000

    show index.html

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
-var fs = require('fs');
 var express = require('express');
-var buf = Buffer(255);
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', buf.toString('utf-8')));
+  var fs = require('fs');
+  var buf = fs.readFileSync('index.html', 'utf-8');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
