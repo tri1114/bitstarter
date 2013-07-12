ae3452a8f4ed2c8879b782c2b2891ab7ce322e40
Tri Sanguanbun <trirhats@gmail.com>
2013-07-12 07:21:32 +0000

    Modified buffer to show index.html

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 var express = require('express');
+var buf = Buffer(255);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', buffer.toString('utf-8')));
+  response.send(fs.readFileSync('index.html', buf.toString('utf-8')));
 });
 
 var port = process.env.PORT || 5000;
